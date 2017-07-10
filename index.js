8095d1b8a85eb1278d9d70895fe1027fadd3e95a
MickeyWang <readmeok@126.com>
2017-07-10 19:35:32 +0800

    添加讲解注释

--- index.js
+++ index.js
@@ -11,12 +11,19 @@ var expressWinston = require('express-winston');
 
 var app = express();
 
+//注意：中间件的加载顺序很重要。
+// 如上面设置静态文件目录的中间件应该放到 routes(app) 之前加载，这样静态文件的请求就不会落到业务逻辑的路由里；
+// flash 中间件应该放到 session 中间件之后加载，因为 flash 是基于 session 的。
+
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'));
 // 设置模板引擎为 ejs
 app.set('view engine', 'ejs');
 
 // 设置静态文件目录
+/*
+ 设置静态文件目录的中间件应该放到 routes(app) 之前加载，这样静态文件的请求就不会落到业务逻辑的路由里
+ */
 app.use(express.static(path.join(__dirname, 'public')));
 // session 中间件
 app.use(session({
@@ -38,6 +45,14 @@ app.use(require('express-formidable')({
   uploadDir: path.join(__dirname, 'public/img'),// 上传文件目录
   keepExtensions: true// 保留后缀
 }));
+
+//在调用 res.render 的时候，express 合并（merge）了 3 处的结果后传入要渲染的模板，
+// 优先级：res.render 传入的对象> res.locals 对象 > app.locals 对象，
+// 所以 app.locals 和 res.locals 几乎没有区别，都用来渲染模板，
+// 使用上的区别在于：
+// app.locals 上通常挂载常量信息（如博客名、描述、作者信息），
+// res.locals 上通常挂载变量信息，即每次请求可能的值都不一样
+// （如请求者信息，res.locals.user = req.session.user）
 
 // 设置模板全局常量
 app.locals.blog = {
--- middlewares/check.js
+++ middlewares/check.js
@@ -1,3 +1,8 @@
+/**
+ * 我们可以把用户状态的检查封装成一个中间件，在每个需要权限控制的路由加载该中间件，即可实现页面的权限控制。
+ *  checkLogin: 当用户信息（req.session.user）不存在，即认为用户没有登录，则跳转到登录页，同时显示 未登录 的通知，用于需要用户登录才能操作的页面及接口
+ *  checkNotLogin: 当用户信息（req.session.user）存在，即认为用户已经登录，则跳转到之前的页面，同时显示 已登录 的通知，如登录、注册页面及登录、注册的接口
+ */
 module.exports = {
   checkLogin: function checkLogin(req, res, next) {
     if (!req.session.user) {
